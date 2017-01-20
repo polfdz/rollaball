4f427b09f9c7ebdd50d0703047feed1d7dddadd9
Ilias <iliaskomp@gmail.com>
2017-01-20 21:32:27 +0100

    Testing lighting/color changes

---
+++ Assets/Scripts/LightAndColor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightAndColor : MonoBehaviour {
+//	float duration;
+//
+//	float t;
+//	Color color1, color2;	// Use this for initialization
+//
+//	void Start () {
+//		duration = 1f;
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		t += Time.deltaTime / duration;
+//		color1 = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+//		color2 = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+//
+//		Color color = Color.Lerp(color1, color2, t);
+//
+//		GetComponent<Renderer> ().material.color = color;
+//	}
+}
